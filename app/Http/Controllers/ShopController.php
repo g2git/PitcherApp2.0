0ad17d04969140ee3632806e1055968b878ce7ee
JvP <jvp@live.nl>
2018-04-06 16:00:59 +0200

    errors repaired

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -16,10 +16,7 @@ class ShopController extends Controller
     {
       $pagination = 9;
       $categories = Category::all();
-<<<<<<< HEAD
-=======
 
->>>>>>> 13e2931375ecac1bee588203ec36b346b95d720b
       if(request()->category){
          $products = Product::with('categories')->whereHas('categories', function ($query) {
          $query->where('slug', request()->category);
@@ -31,10 +28,7 @@ class ShopController extends Controller
          // $categories = Category::all();
          $categoryName = 'Featured';
       }
-<<<<<<< HEAD
-=======
 
->>>>>>> 13e2931375ecac1bee588203ec36b346b95d720b
        //If statement to sort the category page by price
       if(request()->sort == 'low_high'){
         $products = $products->orderBy('price')->paginate($pagination);
@@ -43,10 +37,7 @@ class ShopController extends Controller
       }else{
         $products = $products->paginate($pagination);
       }
-<<<<<<< HEAD
-=======
 
->>>>>>> 13e2931375ecac1bee588203ec36b346b95d720b
       return view('shop')->with([
         'products' => $products,
         'categories' => $categories,
